9fa892b5af2e1353411395c4b988e340bc410773
John Bampfield <johnreidbampfield@example.com>
2026-07-23 14:16:37 -0400

    PLU decomposition and bug fixes

--- examples/main.cpp
+++ examples/main.cpp
@@ -3,23 +3,26 @@
 
 int main() {
 
-    kr::Matrix<float> a(3, 2);
-    kr::Matrix<float> b(2, 3);
+    kr::Matrix<float> a(3, 3);
 
-    a = {1, 2, 
-         3, 4,
-         7, 8};
+    a = {1, 2, 3, 
+         4, 5, 6,
+         7, 8, 9};
 
+    std::cout << a << std::endl << std::endl;
+
+    auto lu = a.lu();
+
+    std::cout << std::endl << std::endl;
+
+    std::cout << lu.U << std::endl << std::endl;
+    std::cout << lu.L << std::endl << std::endl;
+
+    std::cout << lu.P << std::endl << std::endl;
     
-    b = {4, 3, 5,
-         2, 1, 4};
-
-    std::cout << a << std::endl;
-    std::cout << b << std::endl;
-
-    auto c = a * b;
-
-    std::cout << c << std::endl; 
+    std::cout << lu.P.Transpose() << std::endl << std::endl;
+    
+    std::cout << (lu.P.Transpose() * lu.L * lu.U) << std::endl << std::endl;
 
     return 0;
 }
--- include/matrix.hpp
+++ include/matrix.hpp
@@ -8,6 +8,30 @@
 namespace kr {
 
 template <typename T>
+class Matrix;
+
+template <typename T>
+struct LU {
+    Matrix<T> P;
+    Matrix<T> L; 
+    Matrix<T> U;
+
+};
+
+template <typename T>
+Matrix<T> identity(size_t rows_, size_t cols_){
+    Matrix<T> id(rows_, cols_);
+
+    size_t row_;
+
+    for(size_t i = 0; i < cols_; ++i){
+        id(i, i) = 1;
+    }
+
+    return id;
+}
+
+template <typename T>
 class Matrix {
 private:
     std::vector<T> data; 
@@ -19,6 +43,8 @@ public:
     Matrix(size_t rows, size_t cols) : rows_(rows), cols_(cols) {
         data.resize(rows * cols);
     }
+
+    Matrix(){}
 
     Matrix& operator=(std::vector<T> other){
         if(other.size() != data.size()) {throw std::runtime_error("Size does not match"); }
@@ -37,7 +63,7 @@ public:
     }
 
     Matrix operator*(const Matrix& other){
-       if(rows_ != other.cols_){
+       if(cols_ != other.rows_){
             throw std::runtime_error("Size does not match"); 
         }
         
@@ -116,7 +142,7 @@ public:
 
 
     friend std::ostream& operator<<(std::ostream& os, const Matrix& a) {
-        for(size_t i = 0; i < a.data.size(); i++){
+        for(int i = 0; i < a.data.size(); ++i){
             if(i != 0 && i % a.cols_ == 0){
                 std::cout << std::endl;
             }
@@ -126,7 +152,81 @@ public:
         return os;
     }
 
+    void swapRow(size_t r1, size_t r2){
+        for(int i = 0; i < cols_; ++i){
+            std::swap(data[(r1 * cols_) + i], data[(r2 * cols_) + i]);
+        }
+    }
+
+    Matrix<T>& Transpose(){
+        std::vector<T> result(rows_ * cols_);
+        
+        for(size_t i = 0; i < rows_; ++i){
+            for(size_t j = 0; j < cols_; ++j){
+                result[(j * rows_) + i] = data[(i * cols_) + j];
+            }
+        }
+        
+        data = std::move(result); 
+        std::swap(rows_, cols_);
+        
+        return *this;
+    }
     
+    LU<T> lu(){
+        Matrix<T> U(rows_, cols_);
+        Matrix<T> L(rows_, cols_);
+        Matrix<T> P(rows_, cols_);
+        
+        P = identity<T>(rows_, cols_);
+        L = P;
+
+        U = *this;
+
+        std::cout << L << std::endl;
+
+        float epsilon = 1e-5f;
+
+        for(size_t i = 0; i < cols_; ++i){
+            T max = std::fabs(U.data[(i * cols_) + i]);
+
+            size_t maxId = i;
+
+            for(size_t j = i; j < rows_; ++j){
+                T comp = std::fabs(U.data[(j * cols_) + i]);
+                if(comp > max){ max = comp; maxId = j; };
+            }
+
+            U.swapRow(i, maxId);
+            
+            for(size_t row = 0; row < rows_; ++row) {
+                std::swap(P.data[(row * cols_) + i], P.data[(row * cols_) + maxId]);
+            }
+
+            for(size_t col = 0; col < i; ++col) {
+                std::swap(L.data[(i * cols_) + col], L.data[(maxId * cols_) + col]);
+            }
+
+            for(size_t k = i + 1; k < rows_; ++k){
+                T div = U.data[(k * cols_) + i] / U.data[(i * cols_) + i];
+                L.data[(k * cols_) + i] = div;
+
+                for(size_t n = i; n < cols_; ++n){
+                    U.data[(k * cols_) + n] -=  U.data[(i * cols_) + n] * div;
+                }
+            }
+
+        }
+
+        LU<T> result;
+
+        result.U = U;
+        result.L = L;
+        result.P = P;
+
+        return result;
+        
+    }
 
 };
 
